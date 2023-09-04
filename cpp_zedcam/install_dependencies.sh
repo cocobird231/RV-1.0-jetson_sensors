3a4947544af5fa41c308fbe2b39821f2632d5c4a
coco-server <cocobird231@gmail.com>
2023-09-04 16:59:14 +0800

    Fix cpp_zedcam dependencies.

--- cpp_zedcam/install_dependencies.sh
+++ cpp_zedcam/install_dependencies.sh
@@ -69,4 +69,11 @@ echo "Start OpenCV installation..."
 sudo apt install libopencv-dev -y
 
 # Install dependencies
+ubuntu_ver=$(lsb_release -r | grep Release | grep -Po '[\d.]+')
+if [ "$ubuntu_ver" == "18.04" ]
+then
+    echo "Add PPA: 'team-xbmc'"
+    sudo add-apt-repository ppa:team-xbmc/ppa -y
+    sudo apt update
+fi
 sudo apt install nlohmann-json3-dev -y
